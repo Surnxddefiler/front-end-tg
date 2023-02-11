037d0d2cf5f6231cc41949bf9fba927f3443dd93
Еж <easzyXD@gmail.com>
2023-02-11 21:05:18 +0200

    hooks

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,12 +3,11 @@ import Button from "../button/button"
 import style from "./header.module.css"
 
 const Header=()=>{
-    const tg = window.Telegram.WebApp
     const {closeApp}= useTelegram();
     return(
         <header className={style.header}>
         <Button onClick={closeApp}>закрыть</Button>
-        <div>{tg.initDataUnsafe?.user.username}</div>
+        <div>bob</div>
         </header>
     )
 }
