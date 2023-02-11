bcac74b8f345067202976aca8809ccf594ca0113
Еж <easzyXD@gmail.com>
2023-02-11 19:37:46 +0200

    hooks

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,7 +1,7 @@
-import './button.css'
+import style from './button.module.css'
 const Button=(props)=>{
     return(
-        <button {...props} className={"button"+ props.className}/>
+        <button {...props} className={style.button}/>
     )
 }
 export default Button
