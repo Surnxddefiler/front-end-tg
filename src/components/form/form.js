ad2c4d166d063630860c63bd15de0a102cbf2dab
Еж <easzyXD@gmail.com>
2023-02-16 22:47:54 +0200

    form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -11,10 +11,19 @@ const Schedule = () => {
     const [mondaySecond, setMondaySecond] = useState('')
     const [mondayThird, setMondayThird] = useState('')
     const [mondayFourth, setMondayFourth] = useState('')
+
+
     const [mondayFourthCabinet, setMondayFourthCabinet] = useState('')
     const [mondaySecondCabinet, setMondaySecondCabinet] = useState('')
     const [mondayThirdCabinet, setMondayThirdCabinet] = useState('')
     const [mondayFirstCabinet, setMondayFirstCabinet] = useState('')
+
+
+    const [mondayFourthTeacher, setMondayFourthTeacher] = useState('')
+    const [mondaySecondTeacher, setMondaySecondTeacher] = useState('')
+    const [mondayThirdTeacher, setMondayThirdTeacher] = useState('')
+    const [mondayFirstTeacher, setMondayFirstTeacher] = useState('')
+
     const [group, setGroup] = useState('')
     const onSendData = useCallback(() => {
         const data = {
@@ -63,6 +72,8 @@ const Schedule = () => {
     const onChangeMondayFourth = (e) => {
         setMondayFourth(e.target.value)
     }
+
+
     const onChangeMondayFourthCabinet = (e) => {
         setMondayFourthCabinet(e.target.value)
     }
@@ -75,6 +86,23 @@ const Schedule = () => {
     const onChangeMondayFirstCabinet = (e) => {
         setMondayFirstCabinet(e.target.value)
     }
+
+
+
+    const onChangeMondayFourthTeacher = (e) => {
+        setMondayFourthTeacher(e.target.value)
+    }
+    const onChangeMondayThirdTeacher = (e) => {
+        setMondayThirdTeacher(e.target.value)
+    }
+    const onChangeMondaySecondTeacher = (e) => {
+        setMondaySecondTeacher(e.target.value)
+    }
+    const onChangeMondayFirstTeacher = (e) => {
+        setMondayFirstTeacher(e.target.value)
+    }
+
+
     const onChangeGroup = (e) => {
         setGroup(e.target.value)
     }
@@ -85,10 +113,10 @@ const Schedule = () => {
                 <option value={"K-14"}>41-К</option>
                 <option value={"K-13"}>31-K</option>
             </select>
-            <input type="text" placeholder="1 пара" onChange={onChangeMondayFirst} value={mondayFirst} /> <input type="number" placeholder="кабінет" onChange={onChangeMondayFirstCabinet} value={mondayFirstCabinet} />
-            <input type="text" placeholder="2 пара" onChange={onChangeMondaySecond} value={mondaySecond} /> <input type="number" placeholder="кабінет" onChange={onChangeMondaySecondCabinet} value={mondaySecondCabinet} />
-            <input type="text" placeholder="3 пара" onChange={onChangeMondayThird} value={mondayThird} /> <input type="number" placeholder="кабінет" onChange={onChangeMondayThirdCabinet} value={mondayThirdCabinet} />
-            <input type="text" placeholder="4 пара" onChange={onChangeMondayFourth} value={mondayFourth} /> <input type="number" placeholder="кабінет" onChange={onChangeMondayFourthCabinet} value={mondayFourthCabinet} />
+            <input type="text" placeholder="1 пара" onChange={onChangeMondayFirst} value={mondayFirst} /> <input type="number" placeholder="кабінет" onChange={onChangeMondayFirstCabinet} value={mondayFirstCabinet} /> <input type="number" placeholder="кабінет" onChange={onChangeMondayFirstTeacher} value={mondayFirstTeacher} />
+            <input type="text" placeholder="2 пара" onChange={onChangeMondaySecond} value={mondaySecond} /> <input type="number" placeholder="кабінет" onChange={onChangeMondaySecondCabinet} value={mondaySecondCabinet} /> <input type="number" placeholder="кабінет" onChange={onChangeMondaySecondTeacher} value={mondaySecondTeacher} />
+            <input type="text" placeholder="3 пара" onChange={onChangeMondayThird} value={mondayThird} /> <input type="number" placeholder="кабінет" onChange={onChangeMondayThirdCabinet} value={mondayThirdCabinet} /> <input type="number" placeholder="кабінет" onChange={onChangeMondayThirdTeacher} value={mondayThirdTeacher} />
+            <input type="text" placeholder="4 пара" onChange={onChangeMondayFourth} value={mondayFourth} /> <input type="number" placeholder="кабінет" onChange={onChangeMondayFourthCabinet} value={mondayFourthCabinet} /> <input type="number" placeholder="кабінет" onChange={onChangeMondayFourthTeacher} value={mondayFourthTeacher} />
         </form>
     )
 }
