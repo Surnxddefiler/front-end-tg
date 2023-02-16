716e95a7d30fd1b95b681ce66fbf206ecfeb19e7
Еж <easzyXD@gmail.com>
2023-02-16 14:57:19 +0200

    form

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -23,10 +23,10 @@ const Schedule = () => {
                     course: mondaySecond,
                 },
                 {
-                    course: third,
+                    course: mondayThird,
                 },
                 {
-                    course: fourth,
+                    course: mondayFourth,
                 },
             ]
 
