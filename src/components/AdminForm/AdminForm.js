90f96a47f10a63d5f67269b0a103578cf0d9951a
Еж <easzyXD@gmail.com>
2023-02-17 17:23:06 +0200

    form

--- src/components/AdminForm/AdminForm.js
+++ src/components/AdminForm/AdminForm.js
@@ -39,7 +39,7 @@ const AdminForm = () => {
     return (
         <form>
             <div>додати адміна</div>
-            <input type="text" value={changes} placeholder='url на картинку' onChange={onChangeChanges}/>
+            <input type="text" value={changes} placeholder='id користувача' onChange={onChangeChanges}/>
         </form>
     )
 }
