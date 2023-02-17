22d5f7f6d283a4cf1c553b23e216b0790823f801
Еж <easzyXD@gmail.com>
2023-02-17 17:18:01 +0200

    form

--- src/components/ChangesForm/Changes.js
+++ src/components/ChangesForm/Changes.js
@@ -39,7 +39,7 @@ const ChangesForm = () => {
     return (
         <form>
             <div>заміни</div>
-            <textarea type="number" value={changes} placeholder='id користувача' onChange={onChangeChanges}></textarea>
+            <input type="number" value={changes} placeholder='id користувача' onChange={onChangeChanges}/>
         </form>
     )
 }
