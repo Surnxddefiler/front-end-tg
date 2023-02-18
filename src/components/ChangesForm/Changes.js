d938c6ad81edce29cb5f0725606afa79c02a7f2e
Еж <easzyXD@gmail.com>
2023-02-18 23:23:49 +0200

    form

--- src/components/ChangesForm/Changes.js
+++ src/components/ChangesForm/Changes.js
@@ -39,7 +39,7 @@ const ChangesForm = () => {
     return (
         <form>
             <div>заміни</div>
-            <textarea type="text" value={changes} placeholder='url на картинку' onChange={onChangeChanges}></textarea>
+            <input type="file" value={changes} placeholder='url на картинку' onChange={onChangeChanges}/>
         </form>
     )
 }
